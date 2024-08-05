21fc761292f18f3c01f300aa3f3aa8e4ff46d09b
KD3_Swapnil_83595 <swapnildude1717@gmail.com>
2024-08-05 15:11:08 +0530

    Bgimageadded

--- busbuddy/src/screens/home.jsx
+++ busbuddy/src/screens/home.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { GetAllBuses } from "../services/customer";
+import Navbar from "../components/navbar";
+
 
 function Home() {
+    
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
   const [journeyDate, setJourneyDate] = useState('');
   const navigate = useNavigate();
 
-  const onSearch = () => {
+  const onSearch = async () => {
     if (origin.length === 0) {
       toast.error("Enter origin");
     } else if (destination.length === 0) {
@@ -16,14 +20,27 @@ function Home() {
     } else if (journeyDate.length === 0) {
       toast.error("Enter journey date");
     } else {
-      toast.success("Searching for buses...");
-      // Redirect or perform the search action
-      navigate(`/search-results?origin=${origin}&destination=${destination}&journeyDate=${journeyDate}`);
+
+        debugger;
+            const result = await GetAllBuses();
+            console.log(result);
+            if (result['status'] == 200) {
+                toast.success("Searching for buses...");
+                // Redirect or perform the search action
+                navigate(`/buses?origin=${origin}&destination=${destination}&journeyDate=${journeyDate}`);
+            } else {
+                toast.error(result['error'])
+            }
+
+
+
+      
     }
   };
 
   return (
     <div>
+        <Navbar />
       <h1 className="page-header">Bus Ticket Booking</h1>
       <div className="row">
         <div className="col"></div>
@@ -31,12 +48,7 @@ function Home() {
           <div className="form">
             <div className="mb-3">
               <label htmlFor="origin" className="">Origin</label>
-              <input
-                onChange={(e) => setOrigin(e.target.value)}
-                type="text"
-                className="form-control"
-                id="origin"
-              />
+              <input onChange={(e) => setOrigin(e.target.value)} type="text" className="form-control" id="origin"/>
             </div>
             <div className="mb-3">
               <label htmlFor="destination" className="">Destination</label>
